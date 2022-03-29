c0fe069552e124f1d2c5f2e2a739e18ede601bf1
Ethan Marreel <ethan@marreel.com>
2022-03-29 14:08:07 -0700

    Theme and project init

---
+++ dist/main.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+exports.MarenguTheme = void 0;
+var theme_1 = require("./packages/theme");
+exports.MarenguTheme = theme_1.MarenguTheme;
---
+++ dist/theme.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.MarenguTheme = void 0;
+exports.MarenguTheme = {
+    C100: "0B0B0C",
+    C200: "121216",
+    C300: "1A1A1F",
+    C400: "25252D",
+    C500: "F0F1FF",
+    primary: "5CC8FF",
+    secondary: "574AE2"
+};
